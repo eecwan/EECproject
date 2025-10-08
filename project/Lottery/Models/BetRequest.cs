a6e82fe2890a18a563546eb90051dd818aeeea47
eecwan <itinfon@yahoo.com.tw>
2025-10-08 13:05:00 +0800

    Lottery
    上傳

---
+++ project/Lottery/Models/BetRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public class BetRequest
+    {
+        public List<int> Numbers { get; set; } = new List<int>();
+        public int Bet2 { get; set; }
+        public int Bet3 { get; set; }
+        public int Bet4 { get; set; }
+        public int TotalAmount { get; set; }
+    }
+}
