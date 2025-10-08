59f5b0ab0b30a5ee990b9272e244ddc734fdcf7a
eecwan <itinfon@yahoo.com.tw>
2025-10-08 13:06:22 +0800

    上傳
    新上傳

---
+++ project/Lobby/Controllers/BetController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class BetController : Controller
+    {
+        private readonly DrawService _drawService;
+
+        public BetController()
+        {
+            _drawService = new DrawService();
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(); // 對應 Views/Bet/Index.cshtml
+        }
+
+        [HttpPost("Bet/SubmitBet")]
+        public IActionResult SubmitBet([FromBody] BetRequest bet)
+        {
+            if (bet == null || bet.Numbers.Count < 2)
+                return BadRequest(new { error = "至少選 2 個號碼" });
+
+            var result = _drawService.Evaluate(bet);
+            return Json(result);
+        }
+    }
+}
