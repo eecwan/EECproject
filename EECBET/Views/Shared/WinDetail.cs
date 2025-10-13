ac75ac98fedc743a23d1e47b0aa739ed84206baf
KK1513 <zlbtking@gmail.com>
2025-10-13 15:39:40 +0800

    上傳lottery

---
+++ EECBET/Views/Shared/WinDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace EECBET.Models
+{
+    public class WinDetail
+    {
+        public int Type { get; set; }              // 二星、三星、四星
+        public int WinningGroups { get; set; }     // 中獎組數
+        public int PayoutPerGroup { get; set; }    // 每組賠率
+        public int TotalPayout { get; set; }       // 總派彩
+    }
+}
