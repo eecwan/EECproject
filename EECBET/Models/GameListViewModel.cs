2a8e2091b57beda41d76e29b4306dcf43e06f069
eecwan <itinfon@yahoo.com.tw>
2025-10-22 09:28:22 +0800

    slot game

--- EECBET/Models/GameListViewModel.cs
+++ EECBET/Models/GameListViewModel.cs
@@ -3,11 +3,12 @@
     public class GameListViewModel
     {
         public int GameID { get; set; }
-        public string GameNameTW { get; set; }
-        public string GameNameEN { get; set; }
-        public string GameCode { get; set; }
-        public string GameCategory { get; set; }
+        public string? GameNameTW { get; set; }
+        public string? GameNameEN { get; set; }
+        public string? GameCode { get; set; }
+        public string? GameCategory { get; set; }
         public bool IsPromoted { get; set; }
         public bool IsActive { get; set; }
+        public string? GameImageUrl { get; set; }
     }
 }
